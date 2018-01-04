3d23a9ba0adaa24030239ed62790061435207e60
Unknown <david.hm.1994@gmail.com>
2018-01-04 18:07:00 +0100

    Added model holder script

---
+++ Assets/Scripts/ModelHolder.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+public class ModelHolder : MonoBehaviour
+{
+    //Cars
+    public Mesh redCarNormal, redCarWinter;
+    public Mesh blueCarNormal, blueCarWinter;
+    public Mesh greenCarNormal, greenCarWinter;
+    //Trucks
+    public Mesh redTruckNormal, redTruckWinter;
+    public Mesh blueTruckNormal, blueTruckWinter;
+    public Mesh greenTruckNormal, greenTruckWinter;
+    //Road slabs
+    public Mesh plainRoadNormal, plainRoadWinter;
+    public Mesh backStripNormal, backStripWinter;
+    public Mesh forwardStripNormal, forwardStripWinter;
+    public Mesh bothStripNormal, bothStripWinter;
+    //Grass
+    public Mesh clearGrassNormal, clearGrassWinter;
+    public Mesh darkGrassNormal, darkGrassWinter;
+    //Water
+    public Mesh clearWaterNormal, clearWaterWinter;
+    public Mesh darkWaterNormal, darkWaterWinter;
+    //Boulder
+    public Mesh boulderNormal, boulderWinter;
+    //Tree
+    public Mesh treeNormal, treeWinter;
+    //Train
+    public Mesh trainLocomotiveNormal, trainLocomotiveWinter;
+    public Mesh trainWagonNormal, trainWagonWinter;
+    //Trunk
+    public Mesh trunkSmallNormal, trunkSmallWinter;
+    public Mesh trunkMediumNormal, trunkMediumWinter;
+    public Mesh trunkLargeNormal, trunkLargeWinter;
+
+    public Mesh Car
+    {
+        get
+        {
+            return redCarNormal;
+        }
+    }
+
+    public Mesh Truck
+    {
+        get
+        {
+            return redCarNormal;
+        }
+    }
+
+    public Mesh RoadClear
+    {
+        get
+        {
+            return redCarNormal;
+        }
+    }
+
+    public Mesh RoadForwardStrip
+    {
+        get
+        {
+            return redCarNormal;
+        }
+    }
+
+    public Mesh RoadBackwardStrip
+    {
+        get
+        {
+            return redCarNormal;
+        }
+    }
+
+    public Mesh RoadBothStrip
+    {
+        get
+        {
+            return redCarNormal;
+        }
+    }
+
+    public Mesh GrassClear
+    {
+        get
+        {
+            return redCarNormal;
+        }
+    }
+
+    public Mesh GrassDark
+    {
+        get
+        {
+            return redCarNormal;
+        }
+    }
+
+    public Mesh WaterClear
+    {
+        get
+        {
+            return redCarNormal;
+        }
+    }
+
+    public Mesh WaterDark
+    {
+        get
+        {
+            return redCarNormal;
+        }
+    }
+
+    public Mesh Boulder
+    {
+        get
+        {
+            return redCarNormal;
+        }
+    }
+
+    public Mesh Tree
+    {
+        get
+        {
+            return redCarNormal;
+        }
+    }
+
+    public Mesh TrainLocomotive
+    {
+        get
+        {
+            return redCarNormal;
+        }
+    }
+
+    public Mesh TrainWagon
+    {
+        get
+        {
+            return redCarNormal;
+        }
+    }
+
+    public Mesh Trunk
+    {
+        get
+        {
+            return redCarNormal;
+        }
+    }
+}
+
